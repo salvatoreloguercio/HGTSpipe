42ebf3919e9b5c726d71aa5784d550e6e7907637
Salvatore Loguercio <salvatore.loguercio@gmail.com>
2017-10-19 21:38:32 -0700

    added helper scripts for hpc jobs

---
+++ run_HGTS_processing_minimal_RNA.sh
@@ -0,0 +1,17 @@
+
+jobname=`echo R_$1 | cut -c1-15`
+
+echo "#PBS -N ${jobname}"
+echo '#PBS -l nodes=1:ppn=8'
+echo '#PBS -l mem=30gb'
+echo '#PBS -l walltime=240:00:00'
+echo '#PBS -l cput=9600:00:00'
+echo '#PBS -j oe'
+echo '#PBS -m ea'
+echo '#PBS -M ekleiman@scripps.edu'
+echo
+echo 'cd $PBS_O_WORKDIR'
+echo 'module load R'
+echo
+echo "Rscript --no-restore --no-save HGTS_processing_minimal.R -i $1 -t RNA -d $2"
+echo
